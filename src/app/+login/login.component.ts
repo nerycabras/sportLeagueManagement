2dbefcb03512b66f6e0f2c783dcf8cc6ff316f26
nerycabras <nery.210689@gmail.com>
2016-08-04 22:01:46 -0500

    se agregó algunos paquete de angular material

--- src/app/+login/login.component.ts
+++ src/app/+login/login.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {  UsuarioService } from '../service';
 import { Router } from '@angular/router';
-
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
 
 @Component({
   moduleId: module.id,
   selector: 'app-login',
   templateUrl: 'login.component.html',
   styleUrls: ['login.component.css'],
-  providers: [UsuarioService]
+  providers: [UsuarioService],
+  directives: [MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES]
 })
 export class LoginComponent implements OnInit {
   usuario="local user";
--- src/system-config.ts
+++ src/system-config.ts
@@ -3,12 +3,27 @@
  **********************************************************************************************/
 /** Map relative paths to URLs. */
 const map: any = {
+    '@angular2-material': 'vendor/@angular2-material'
 };
 
 /** User packages configuration. */
+/*const packages: any = {
+  '@angular2-material/core':{main:'core.js'},
+  '@angular2-material/button':{main:'button.js'},
+  '@angular2-material/card':{main:'card.js'}
+};
+*/
+
 const packages: any = {
 };
-
+const materialPkgs:string[] = [
+  'core',
+  'button',
+  'card',
+];
+materialPkgs.forEach((pkg) => {
+  packages[`@angular2-material/${pkg}`] = {main: `${pkg}.js`};
+});
 ////////////////////////////////////////////////////////////////////////////////////////////////
 /***********************************************************************************************
  * Everything underneath this line is managed by the CLI.
